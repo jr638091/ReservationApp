d110ca143770511136f785448a5bb5894bdd02d8
jr638091 <jr638091@gmail.com>
2020-12-29 18:23:02 -0500

    Clean main branch

--- Models/ReservationContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace ReservationApp.Models
-{
-    public class ReservationContext: DbContext {
-        public ReservationContext (DbContextOptions<ReservationContext> options): base(options){}
-        public DbSet<ContactType> ContactTypes { get; set;}
-    }
-}
